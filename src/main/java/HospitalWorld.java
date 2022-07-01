d7e93608c1191341a0a457a694e9ab4c139ace69
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-01 13:46:47 -0400

    removed useless imports

--- src/main/java/HospitalWorld.java
+++ src/main/java/HospitalWorld.java
@@ -1,12 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
 
 import doctor.Doctor;
 import hospital.Hospital;
